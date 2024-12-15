68bf0e13033cb9853a6d1357ba34b7373e5a794e
Mikita Puntus <150572042+mpuntus-css@users.noreply.github.com>
2024-12-15 15:00:02 -0600

    some system updates

--- obj/Release/net8.0/Final_Project.AssemblyInfo.cs
+++ obj/Release/net8.0/Final_Project.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Final_Project")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97e37d14e9130891a3775fe26bf068c474bee2dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Final_Project")]
